9bc3fca437b2e7edadf70b84d19e41f279c6e757
Ducrot Aurelie <ducrotaurelie@Ducrots-MacBook-Pro.local>
2017-10-29 17:25:42 +0100

    unworking code for final simulation

--- main.cpp
+++ main.cpp
@@ -1,59 +1,78 @@
-#include "neuron.cpp"
-//#include <random>
+#include "neuron.hpp"
+#include <iostream>
+#include <cassert>
+#include <random>
 
 using namespace std;
+
+void random_connection(vector<Neuron> neuron_sim, int const CE, int const CI, int const nb_exc, int const nb_inh);
 
 int main()
 {
 	double t_stop (1000);
 	double h (0.1);
 	double simtime (0);
-	double Iext1;
-	double Iext2;
-	//auto Iext=unifrom_int_distribution<> (0,400);
-	cout<< "what is the intensity of the current Iext for the 1st neuron? the value should be between 0 and 400 pA."<< endl;
-	//gestion d'erreur a ajouter
-	cin>> Iext1;
-	cout<< "what is the intensity of the current Iext for the 1st neuron? the value should be between 0 and 400 pA."<< endl;
-	//gestion d'erreur a ajouter
-	cin>> Iext2;
-	//double threshold (20);
+	int number_Neuron_sim;
+	cout << "how many neurons must be generated?" << endl;
+	cin >> number_Neuron_sim;
+	int const Excitory_neuron_nb (0.8*number_Neuron_sim);
+	int const Inhibitory_neuron_nb (0.2*number_Neuron_sim);
+	int const CE (0.1*Excitory_neuron_nb);
+	int const CI (0.1*Inhibitory_neuron_nb);
 
-	Neuron N_1;
-	Neuron N_2;
-	double n;
-	n=(N_1.getTauxRefractory()/h);
-  ofstream out1;
-	ofstream out2;
-  out1.open("potential1.txt");
-	out2.open("potential2.txt");
-	bool hh (true);
+	vector< Neuron > Neurons (number_Neuron_sim); //! vector of all neurons in simulation
 
-    if (out1.fail())
-    {
-        cout<<"Erreur, the file was not opened"<<endl;
-    } else if (out2.fail())
+	//! initialisation of the neurons in the simulation: exhibitory and inhibitory
+	for (int i(0); i< Excitory_neuron_nb; ++i)
+	{
+		Neuron n;
+		Neurons.push_back(n);
+	}
+
+	for (int j(0); j< Inhibitory_neuron_nb; ++j)
+	{
+		Neuron n;
+		Neurons.push_back(n);
+	}
+	cout<< "the neuron simulation tableau has been generated"<<endl;
+
+	//! making the connections between the neurons
+	random_connection(Neurons,CE,CI,Excitory_neuron_nb,Inhibitory_neuron_nb);
+	cout<< "the connections have been made"<<endl;
+
+	while (simtime<t_stop)
+	{
+		for (int i(0); i<Neurons.size(); i++)
 		{
-			cout<<"Erreur, the file was not opened"<<endl;
-		} else {
-
-      while (simtime<t_stop)
-        {
-            N_1.update(Iext1,simtime,N_2.getisSpiking());
-						out1<<N_1.getPotential()<<endl;
-						cout<< "N1 spiking status:" << N_1.getisSpiking()<<endl;
-						N_2.update(Iext2,simtime,N_1.getisSpiking());
-						out2<<N_2.getPotential()<<endl;
-						cout<< "N2 spiking status:" << N_2.getisSpiking()<<endl;
-          	simtime +=h;
-        }
-
-        cout << "the neuron 1 spiked :"<<N_1.getSpikecount()<<" times"<< endl;
-				cout<< "the neuron 2 spiked :"<<N_2.getSpikecount()<<" times"<<endl;
-    }
-
-	out1.close();
-	out2.close();
-
+			Neurons[i].update(simtime);
+		}
+		simtime+=h;
+	}
+	cout<< "end of program"<<endl;
 	return 0;
+}
+
+void random_connection(vector <Neuron> neuron_sim, int const CE, int const CI, int const nb_exc, int const nb_inh)
+{
+	for (int i(0); i< neuron_sim.size(); ++i)
+	{
+		vector <Neuron> connexion_matrix (CE+CI);
+		for (int j(0); j< CE; j++)
+		{
+			random_device rd;
+			mt19937 eng(rd());
+			uniform_int_distribution<int> distr(0,nb_exc);
+			connexion_matrix.push_back(neuron_sim[distr(eng)]);
+			neuron_sim[j].setConnexion(connexion_matrix);
+		}
+		for (int k(0); k<CI; k++)
+		{
+			random_device rd;
+			mt19937 eng(rd());
+			uniform_int_distribution<int> distr(nb_exc+1,nb_exc+nb_inh);
+			connexion_matrix.push_back(neuron_sim[distr(eng)]);
+			neuron_sim[k].setConnexion(connexion_matrix);
+		}
+	}
+
 }
--- neuron.cpp
+++ neuron.cpp
@@ -1,32 +1,15 @@
 #include "neuron.hpp"
 #include <iostream>
 #include <cmath>
+#include <random>
 
 /*Constructeur et destructeur*/
 
-Neuron::Neuron (): potential_(0), spikecount_(0),spikeTime_(0), taux_(20),tauxRefractory_(2), h_(0.1),
+Neuron::Neuron (): potential_(0), spikecount_(0),spikeTime_(0), taux_(20),tauxRefractory_(2), h_(0.1), Iext_(0),
 							ref_time_(tauxRefractory_/h_), conductivity_(1), Res_(taux_/conductivity_),
-							threshold_(20), isSpiking_(false),clock_(0),Delay_(15), SumSpike_(Delay_,0), J_(0.1)
+							threshold_(20), isSpiking_(false),clock_(0),Delay_(15), Buffer_(Delay_+1,0), J_(0.1)
 							//,connected_with_(0)
 {}
-
-
-Neuron::Neuron (Neuron const& other) // a modifier avec le reste des new values
-{
-	potential_= other.potential_;
-	spikecount_=other.spikecount_;
-	spikeTime_=other.spikeTime_;
-	taux_=other.taux_;
-	tauxRefractory_ =other.tauxRefractory_;
-	Res_=other.Res_;
-	conductivity_=other.conductivity_;
-	isSpiking_=other.isSpiking_;
-	clock_=other.clock_;
-	Delay_=other.Delay_;
-	SumSpike_=other.SumSpike_;
-	J_=other.J_;
-
-}
 
 Neuron::~Neuron()
 {}
@@ -112,62 +95,56 @@ const int Neuron::getClock()
 	return clock_;
 }
 
-//methodes
-
-void Neuron::isSpiking_check(double const& Iext, double const& simtime)
+void Neuron::setConnexion (vector <Neuron>const& connexion_matrix)
 {
-	potential_= (exp(-h_/taux_)*potential_)+Iext*Res_*(1-exp(-h_/taux_));
-	if (potential_> threshold_)
-	{
-		isSpiking_=true;
-		spikecount_=spikecount_+1;
-		setSpikeTime(simtime);
-		potential_= 0;
-		ref_time_= tauxRefractory_/h_;
-	} else {
-		isSpiking_=false;
-	}
-
+	connected_with_=connexion_matrix;
 }
 
-void Neuron::update(double const& Iext, double& simtime, bool ispiking)
+void Neuron::setBuffer()
 {
-	if (ispiking==true)
+	Buffer_[(clock_+Delay_)%Buffer_.size()]+=1;
+}
+
+//methodes
+void Neuron::give_spike()
+{
+	if (isSpiking_)
 	{
-		SumSpike_[0]+=1;
+		for (int i(0); i<connected_with_.size(); ++i)
+		{
+			connected_with_[i].setBuffer();
+		}
 	}
+}
 
-	for (int i(Delay_-1); i>=1 ; --i)
+void Neuron::update(double& simtime)
+{
+	double lambda(0.005);
+	random_device rd;
+	mt19937 gen(rd());
+	poisson_distribution<> p(lambda);
+
+	double background_ (static_cast<double> (p(gen))); //intialise the background stimulation every time tep
+
+	if (ref_time_>=0)
 	{
-		SumSpike_[i]= SumSpike_[i-1];
-	}
-
-	SumSpike_[0]=0;
-
-	if (ref_time_>0)
-	{
-		isSpiking_=true;
 		potential_=0;
 		ref_time_-=1;
-	} else if (ispiking==true)
+		isSpiking_=false; // a verifier.... essayer true si ca marche pas...
+	} else {
+		potential_= (exp(-h_/taux_)*potential_)+Iext_*Res_*(1-exp(-h_/taux_))+Buffer_[clock_]*J_+background_;
+		Buffer_[clock_]=0;
+		spikecount_+=1;
+		setSpikeTime(simtime);
+		ref_time_= tauxRefractory_/h_;
+
+		if(potential_>threshold_)
 		{
-			if (clock_>=Delay_) // if never spiked ans is already greater then delay
-			{
-				int x=SumSpike_[Delay_-1];// takes last value in table
-				potential_= (exp(-h_/taux_)*potential_)+Iext*Res_*(1-exp(-h_/taux_)) + x*J_;
-				SumSpike_[Delay_-1]=0;
-				clock_=0;
-
-				if (potential_>threshold_)
-				{
-					isSpiking_=true;
-					spikecount_=spikecount_+1;
-					setSpikeTime(simtime);
-					potential_= 0;
-					ref_time_= tauxRefractory_/h_;
-				}
-			} else { isSpiking_check(Iext,simtime); }
-		} else { isSpiking_check(Iext,simtime); }
-
+			isSpiking_=true;
+			give_spike();
+		} else {
+			isSpiking_=false;
+		}
+	}
 		clock_+=1;
 }
